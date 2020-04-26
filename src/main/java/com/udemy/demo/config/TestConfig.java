f3684b71acd2eb4d227df34561d8f3d4696e1472
grizzlyPi <lucasrodriguesdelima@rocketmail.com>
2020-04-25 22:07:20 -0300

    Payment, one-to-one association

--- src/main/java/com/udemy/demo/config/TestConfig.java
+++ src/main/java/com/udemy/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.udemy.demo.entities.Category;
 import com.udemy.demo.entities.Order;
 import com.udemy.demo.entities.OrderItem;
+import com.udemy.demo.entities.Payment;
 import com.udemy.demo.entities.Product;
 import com.udemy.demo.entities.User;
 import com.udemy.demo.entities.enums.OrderStatus;
@@ -38,7 +39,7 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderItemRepository orderItemRepository;
-
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -81,5 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:58:45Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
